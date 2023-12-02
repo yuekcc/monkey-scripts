0e749473fda58a7fbd8bceccdad36c0e053cd1d0
yuekcc <yuekcc@qq.com>
2023-12-02 22:26:05 +0800

    兼容 Violentmonkey

--- scripts/export_v2ex_thread.js
+++ scripts/export_v2ex_thread.js
@@ -1,102 +1,105 @@
 // ==UserScript==
-// @name         导出
-// @namespace    http://tampermonkey.net/
+// @name         V2EX 导出
+// @namespace   Violentmonkey Scripts
 // @version      0.1
 // @description  导出 V2ex 帖子为 Markdown
 // @author       zhanlefeng
 // @match        https://v2ex.com/t/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=v2ex.com
-// @grant        none
-// @run-at       context-menu
+// @grant        GM_registerMenuCommand
 // ==/UserScript==
 
 !(function () {
-  function printMarkdown(threads) {
-    return threads
-      .map((thread, index) => {
-        const headerLevel = index === 0 ? 1 : 2;
+  GM_registerMenuCommand('导出讨论贴', doStuff);
 
-        if (headerLevel === 1) {
-          const lines = [
-            `#`.repeat(headerLevel) + ' ' + thread.title,
-            `原文地址：[${location.href}](${location.href})`,
-            `#`.repeat(headerLevel + 1) + ` _${thread.time}_ ${thread.author}`,
-            thread.content,
-          ];
+  function doStuff() {
+    function printMarkdown(threads) {
+      return threads
+        .map((thread, index) => {
+          const headerLevel = index === 0 ? 1 : 2;
+
+          if (headerLevel === 1) {
+            const lines = [
+              `#`.repeat(headerLevel) + ' ' + thread.title,
+              `原文地址：[${location.href}](${location.href})`,
+              `#`.repeat(headerLevel + 1) + ` _${thread.time}_ ${thread.author}`,
+              thread.content,
+            ];
+            return lines.join('\n\n');
+          }
+
+          const lines = [`#`.repeat(headerLevel) + ` _${thread.time}_ ${thread.author}`, thread.content];
           return lines.join('\n\n');
-        }
-
-        const lines = [`#`.repeat(headerLevel) + ` _${thread.time}_ ${thread.author}`, thread.content];
-        return lines.join('\n\n');
-      })
-      .join('\n\n');
-  }
-
-  function parseHtmlToStr(innerHtml) {
-    const p = new DOMParser();
-    const doc = p.parseFromString(innerHtml, 'text/html');
-
-    const result = [];
-
-    doc.body.childNodes.forEach(node => {
-      if (node.tagName === 'BR') {
-        result.push('\n\n');
-        return;
-      }
-
-      result.push(node.textContent);
-    });
-
-    return result.join('');
-  }
-
-  function parseReplyThread(el) {
-    const boxes = [...el.querySelectorAll(`.cell td[align='left']`)];
-    if (boxes.length === 0) {
-      return [];
+        })
+        .join('\n\n');
     }
 
-    return boxes.map(box => {
-      const author = box.querySelector('strong')?.textContent;
-      const time = box.querySelector('.ago')?.title;
-      const content = parseHtmlToStr(box.querySelector('.reply_content').innerHTML);
+    function parseHtmlToStr(innerHtml) {
+      const p = new DOMParser();
+      const doc = p.parseFromString(innerHtml, 'text/html');
+
+      const result = [];
+
+      doc.body.childNodes.forEach(node => {
+        if (node.tagName === 'BR') {
+          result.push('\n\n');
+          return;
+        }
+
+        result.push(node.textContent);
+      });
+
+      return result.join('');
+    }
+
+    function parseReplyThread(el) {
+      const boxes = [...el.querySelectorAll(`.cell td[align='left']`)];
+      if (boxes.length === 0) {
+        return [];
+      }
+
+      return boxes.map(box => {
+        const author = box.querySelector('strong')?.textContent;
+        const time = box.querySelector('.ago')?.title;
+        const content = parseHtmlToStr(box.querySelector('.reply_content').innerHTML);
+
+        return {
+          author,
+          time,
+          content,
+        };
+      });
+    }
+
+    function parseMainThread(tempDiv) {
+      var titleElement = tempDiv.querySelector('h1');
+      var title = titleElement.textContent.trim();
+      var usernameElement = tempDiv.querySelector('.header .gray a[href^="/member/"]');
+      var author = usernameElement.textContent.trim();
+      var timestampElement = tempDiv.querySelector('.header .gray span');
+      var time = timestampElement.getAttribute('title');
+      var contentElement = tempDiv.querySelector('.topic_content');
+      var content = parseHtmlToStr(contentElement.innerHTML);
 
       return {
+        title,
         author,
         time,
         content,
       };
-    });
+    }
+
+    const threadBoxes = [...document.querySelector('#Main').querySelectorAll('.box')];
+    const [mainThreadBox, ...otherBoxes] = threadBoxes;
+
+    const result = [];
+    result.push(parseMainThread(mainThreadBox));
+    otherBoxes
+      .map(parseReplyThread)
+      .flat()
+      .forEach(it => result.push(it));
+
+    const doc = printMarkdown(result);
+    navigator.clipboard.writeText(doc).then(() => alert('已复制到粘贴板'));
   }
-
-  function parseMainThread(tempDiv) {
-    var titleElement = tempDiv.querySelector('h1');
-    var title = titleElement.textContent.trim();
-    var usernameElement = tempDiv.querySelector('.header .gray a[href^="/member/"]');
-    var author = usernameElement.textContent.trim();
-    var timestampElement = tempDiv.querySelector('.header .gray span');
-    var time = timestampElement.getAttribute('title');
-    var contentElement = tempDiv.querySelector('.topic_content');
-    var content = parseHtmlToStr(contentElement.innerHTML);
-
-    return {
-      title,
-      author,
-      time,
-      content,
-    };
-  }
-
-  const threadBoxes = [...document.querySelector('#Main').querySelectorAll('.box')];
-  const [mainThreadBox, ...otherBoxes] = threadBoxes;
-
-  const result = [];
-  result.push(parseMainThread(mainThreadBox));
-  otherBoxes
-    .map(parseReplyThread)
-    .flat()
-    .forEach(it => result.push(it));
-
-  const doc = printMarkdown(result);
-  navigator.clipboard.writeText(doc).then(() => alert('已复制到粘贴板'));
 })();
